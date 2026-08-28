81ab897bbc50237de23c9eb4dbb04c77ecad78a1
loveshack <ivangklef@gmail.com>
2026-08-28 01:52:56 -0700

    fix: refresh shell cache after removing contact form

--- shared/components/load-basics/load-basics.js
+++ shared/components/load-basics/load-basics.js
@@ -279,7 +279,7 @@
     try {
       // Fetch the component HTML
       const componentUrl =
-        componentBaseUrl + CONFIG.componentFileName + "?v=5";
+        componentBaseUrl + CONFIG.componentFileName + "?v=6";
       //console.log("[LoadBasics] Fetching:", componentUrl);
 
       const response = await fetch(componentUrl);
